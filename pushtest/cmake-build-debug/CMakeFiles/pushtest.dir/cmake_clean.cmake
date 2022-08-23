177e6e3773e0aa5ffe3b36b2d15fc41a080d242c
shawn <shawnfeng610@gmail.com>
2022-08-24 04:16:04 +0800

    create pushtest

---
+++ pushtest/cmake-build-debug/CMakeFiles/pushtest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pushtest.dir/main.c.obj"
+  "libpushtest.dll.a"
+  "pushtest.exe"
+  "pushtest.exe.manifest"
+  "pushtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pushtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pushtest/cmake-build-debug/CMakeFiles/pushtest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pushtest.
+# This may be replaced when dependencies are built.
