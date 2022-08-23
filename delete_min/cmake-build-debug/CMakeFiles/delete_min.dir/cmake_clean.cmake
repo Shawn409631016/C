1cc84fc52126f5c33e6bdd3e99e4ce9c6c994ea4
shawn <shawnfeng610@gmail.com>
2022-08-24 05:15:30 +0800

    create delete_min

---
+++ delete_min/cmake-build-debug/CMakeFiles/delete_min.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/delete_min.dir/main.c.obj"
+  "delete_min.exe"
+  "delete_min.exe.manifest"
+  "delete_min.pdb"
+  "libdelete_min.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/delete_min.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
